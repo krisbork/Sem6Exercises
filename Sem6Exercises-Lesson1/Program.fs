f129d463eae4ffce33e635cfe1e9f6c4609ed106
krisbork <70579756+krisbork@users.noreply.github.com>
2022-02-01 11:48:39 +0100

    Added three exercises

--- Sem6Exercises-Lesson1/Program.fs
+++ Sem6Exercises-Lesson1/Program.fs
@@ -1,4 +1,16 @@
+module Lesson1
 
-
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+//1.2
+let h (x: float, y:float) =
+    sqrt (x**2+y**2)
+    
+//1.5
+let rec f x =
+    if x = 0 then 0
+    elif x = 1 then 1
+    else f(x-1) + f(x-2)
+    
+//1.6
+let rec sum(m,n) =
+    if n=0 then m + (m+1) + (m+2)
+    else m + (m+(n-1)) + (m+n) + sum(m, n-1)
